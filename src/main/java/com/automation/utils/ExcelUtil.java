e30bb2036ea9f426c1851e86c6fa392ad5de3bbf
Prashant <prashant21box@gmail.com>
2022-09-08 21:46:10 +0530

    ExcelUtil.java file added into the project to read the excel file and convert the data in excel file into two dimensional object array.

---
+++ src/main/java/com/automation/utils/ExcelUtil.java
@@ -0,0 +1,46 @@
+package com.automation.utils;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class ExcelUtil {
+    private static final String TEST_DATA_SHEET_PATH = System.getProperty("user.dir") + "//src//test//resources//testdata//TestData.xlsx";
+    private static Workbook workbook;
+    private static Sheet sheet;
+
+    public static Object[][] getTestData(String sheetName) {
+        Object data[][] = null;
+
+        try {
+            FileInputStream inputStream = new FileInputStream(TEST_DATA_SHEET_PATH);
+            workbook = WorkbookFactory.create(inputStream);
+            sheet = workbook.getSheet(sheetName);
+
+            data = new Object[sheet.getLastRowNum()][sheet.getRow(0).getLastCellNum()];
+
+            for (int i = 0; i < sheet.getLastRowNum(); i++) {
+                for (int j = 0; j < sheet.getRow(0).getLastCellNum(); j++) {
+                    data[i][j] = sheet.getRow(i + 1).getCell(j).toString();
+                }
+            }
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (InvalidFormatException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return data;
+    }
+
+
+}
